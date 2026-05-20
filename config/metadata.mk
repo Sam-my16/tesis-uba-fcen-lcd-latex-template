1963932937b732026eea6ac9bde2271adaac7de9
Sam-my16 <sivch2012@gmail.com>
2026-05-20 14:08:34 -0300

    realicé una prueba para verificar que anda

--- config/metadata.mk
+++ config/metadata.mk
@@ -2,6 +2,6 @@
 # Se pueden sobrescribir desde la linea de comandos:
 # make PROJECT=manuscrito STUDENT_LAST=Perez STUDENT_NAME=Ana STUDENT_LU=123_45
 
-STUDENT_LAST ?= Apellido
-STUDENT_NAME ?= Nombre
-STUDENT_LU   ?= 000_00
+STUDENT_LAST ?= Vallejo
+STUDENT_NAME ?= Sammy
+STUDENT_LU   ?= 664_21
